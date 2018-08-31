7ff88ce2d5a7a0f8672e26e59e68c23d9553efa1
UramnOIL <uramnoil@outlook.com>
2018-08-31 22:19:28 +0900

    完成

---
+++ src/VectorNetworkProject/FileStore/FileStore.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UramnOIL
+ * Date: 2018/08/31
+ * Time: 22:17
+ */
+
+namespace VectorNetworkProject\FileStore;
+
+
+class FileStore extends PluginBase
+{
+
+}
